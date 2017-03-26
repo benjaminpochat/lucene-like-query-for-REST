f8d6cd59d09eff5ea850e4c4bcd556146c7c7ffc
Benjamin POCHAT <benjaminpochat@pc12.home>
2017-03-26 22:35:12 +0200

    OData conversion feature

--- src/Converter.js
+++ src/Converter.js
@@ -1,15 +1,21 @@
 /**
  * A Converter responsible for converting a natural query into a REST query (in FIQL format).
  * 
- * The REST query is formatted as a FIQL query : https://tools.ietf.org/html/draft-nottingham-atompub-fiql-00
+ * Depending on the destinationFormat ("ODATA" or "FIQL"), the REST query is formatted as :
+ * - a OData query : http://docs.oasis-open.org/odata/odata/v4.0/cos01/part2-url-conventions/odata-v4.0-cos01-part2-url-conventions.html#_Toc372793792
+ * - or a FIQL query : https://tools.ietf.org/html/draft-nottingham-atompub-fiql-00
  * 
- * TODO : implement a solution to deal with OData query format : http://docs.oasis-open.org/odata/odata/v4.0/cos01/part2-url-conventions/odata-v4.0-cos01-part2-url-conventions.html#_Toc372793792
+ * If destinationFormat is not defined, OData format is used.
+ * 
  */
 
-var Converter = function( selector, attributes ){
+var Converter = function( selector , attributes , destinationFormat){
+	const ODATA = "ODATA";
+	const FIQL = "FIQL";
+	this.destinationFormat = destinationFormat == undefined ? ODATA : destinationFormat; 
 	this.attributes = attributes;
 	this.parser = new Parser();
-	
+
 	/**
 	 * Main function : converts the natural query which is the value of input field pointed by the selector
 	 */
@@ -56,15 +62,15 @@ var Converter = function( selector, attributes ){
 	};
 
 	this.convertSimpleEqualCondition = function( naturalCondition ){
-		return this.convertSimpleCondition( naturalCondition, ":", "==" );
+		return this.convertSimpleCondition( naturalCondition, ":", this.getEqualRestToken() );
 	};
 
 	this.convertGreaterOrEqualThanCondition = function( naturalCondition ){
-		return this.convertSimpleCondition( naturalCondition, ">", "=ge=" );
+		return this.convertSimpleCondition( naturalCondition, ">", this.getGreaterOrEqualRestToken() );
 	};
 
 	this.convertLowerOrEqualThanCondition = function( naturalCondition ){
-		return this.convertSimpleCondition(naturalCondition, "<", "=le=");
+		return this.convertSimpleCondition(naturalCondition, "<", this.getLowerOrEqualRestToken());
 	};
 	
 	this.convertSimpleCondition = function( 
@@ -79,29 +85,36 @@ var Converter = function( selector, attributes ){
 	};
 
 	this.getAttribute = function( attributeName ){
-		if(this.attributes === undefined ){
-			return undefined ;
+		if ( this.attributes == undefined || this.attributes == null ) {
+			return null ;
 		}
-		for( i = 0 ; i < this.attributes.length ; i++ ){
-			if(this.attributes[i].naturalName == attributeName){
-				return this.attributes[i];
+		for ( i = 0 ; i < this.attributes.length ; i++ ) {
+			if ( this.attributes[i].naturalName == attributeName ) {
+				return this.attributes[i] ;
 			}
 		}
 	}
 	
 	this.getRestValue = function ( attributeName, naturalValue ) {
 		var attribute = this.getAttribute( attributeName ) ;
-		if(attribute === undefined ) {
-			return naturalValue;
+		var restValue ;
+		if(attribute == undefined || attribute == null ) {
+			restValue =  naturalValue ;
 		}
-		if ( ! attribute.mappedValues ) {
-			return naturalValue;
+		else if ( ! attribute.mappedValues ) {
+			restValue = naturalValue ;
 		} else {
 			for ( i = 0 ; i < attribute.possibleValues.length ; i++ ) {
 				if ( attribute.possibleValues[i].value == naturalValue ) {
-					return attribute.possibleValues[i].key ;
+					restValue = attribute.possibleValues[i].key ;
 				}
 			}
+		}
+		
+		if ( this.destinationFormat == ODATA ) {
+			return "'" + restValue + "'" ;
+		} else {
+			return restValue ;
 		}
 	};
 	
@@ -125,18 +138,79 @@ var Converter = function( selector, attributes ){
 		var searchAttribute = naturalCondition.substring(0, separatorIndex);
 		var searchValueWithSpace = naturalCondition.substring(separatorIndex+2);
 		var searchValueWithSpace = searchValueWithSpace.substring(0, searchValueWithSpace.length - 1);
-		var restCondition = searchAttribute + "==" + searchValueWithSpace;
+		var restCondition = 
+			searchAttribute 
+			+ this.getEqualRestToken() 
+			+ this.getRestValue( searchAttribute , searchValueWithSpace );
 		return restCondition;
 	};
 
-	this.convertRangeCondition = function( naturalCondition ){
-		var separatorIndex = naturalCondition.indexOf(":");
-		var searchAttribute = naturalCondition.substring(0, separatorIndex);
-		var rangeValues = naturalCondition.substring(separatorIndex+2);
-		var rangeValues = rangeValues.substring(0, rangeValues.length - 1);
-		var rangeValuesArray = rangeValues.split(" ");
-		var restCondition = searchAttribute + "=ge=" + rangeValuesArray[0] + ";" + searchAttribute + "=le=" + rangeValuesArray[1]
-		return restCondition;
+	this.convertRangeCondition = function( naturalCondition ) {
+		var separatorIndex = naturalCondition.indexOf(":") ;
+		var searchAttribute = naturalCondition.substring(0, separatorIndex) ;
+		var rangeValues = naturalCondition.substring(separatorIndex+2) ;
+		var rangeValues = rangeValues.substring(0, rangeValues.length - 1) ;
+		var rangeValuesArray = rangeValues.split(" ") ;
+		var restCondition = 
+			searchAttribute 
+			+ this.getGreaterOrEqualRestToken() 
+			+ rangeValuesArray[0] 
+			+ this.getAndRestToken() 
+			+ searchAttribute 
+			+ this.getLowerOrEqualRestToken() 
+			+ rangeValuesArray[1] ;
+		return restCondition ;
 	};	
+	
+	this.getEqualRestToken = function(){
+		switch(this.destinationFormat) {
+		case ODATA :
+			return " eq ";
+		case FIQL :
+			return "==";
+		}
+		return null;
+	} ;
+	
+	
+	this.getGreaterOrEqualRestToken = function(){
+		switch(this.destinationFormat) {
+		case ODATA :
+			return " ge ";
+		case FIQL :
+			return "=ge=";
+		}
+		return null;
+	} ;
+
+	this.getLowerOrEqualRestToken = function(){
+		switch(this.destinationFormat) {
+		case ODATA :
+			return " le ";
+		case FIQL :
+			return "=le=";
+		}
+		return null;
+	} ;
+	
+	this.getAndRestToken = function(){
+		switch(this.destinationFormat) {
+		case ODATA :
+			return " and ";
+		case FIQL :
+			return ";";
+		}
+		return null;
+	} ;
+	
+	this.getOrRestToken = function(){
+		switch(this.destinationFormat) {
+		case ODATA :
+			return " or ";
+		case FIQL :
+			return ",";
+		}
+		return null;
+	} ;	
 	
 };
